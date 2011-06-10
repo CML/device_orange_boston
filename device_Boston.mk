97627788b0fcdd0eca0eb6579f0bc4d99f57c3b5
CML <chaco_Sc_21@hotmail.com>
2011-06-10 21:57:31 +0200

    Version inicial utilizada en SuperAosp ver:8.6

---
+++ device_Boston.mk
@@ -0,0 +1,181 @@
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/orange/Boston/overlay
+
+# HAL libs and other system binaries
+PRODUCT_PACKAGES += \
+    hwprops \
+    abtfilt \
+    gps.Boston \
+    lights.Boston \
+    copybit.Boston \
+    gralloc.Boston \
+    libOmxCore
+
+# Extra apps
+PRODUCT_PACKAGES += \
+	Torch
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+    LOCAL_KERNEL := device/orange/Boston/kernel
+else
+    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+
+# Live wallpaper packages
+PRODUCT_PACKAGES += \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    VisualizationWallpapers \
+    librs_jni
+
+# Publish that we support the live wallpaper feature.
+PRODUCT_COPY_FILES += \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+
+# Board-specific init
+PRODUCT_COPY_FILES += \
+    device/orange/Boston/ueventd.qct.rc:root/ueventd.qct.rc \
+    device/orange/Boston/init.qcom.rc:root/init.qcom.rc
+
+## RIL related stuff
+PRODUCT_COPY_FILES += \
+    device/orange/Boston/spn-conf.xml:system/etc/spn-conf.xml \
+    device/orange/Boston/prebuilt/libril.so:system/lib/libril.so \
+    device/orange/Boston/prebuilt/rild:system/bin/rild \
+    vendor/orange/Boston/proprietary/bin/qmuxd:system/bin/qmuxd \
+    vendor/orange/Boston/proprietary/lib/liboncrpc.so:system/lib/liboncrpc.so \
+    vendor/orange/Boston/proprietary/lib/libmmgsdilib.so:system/lib/libmmgsdilib.so \
+    vendor/orange/Boston/proprietary/lib/libgsdi_exp.so:system/lib/libgsdi_exp.so \
+    vendor/orange/Boston/proprietary/lib/libgstk_exp.so:system/lib/libgstk_exp.so \
+    vendor/orange/Boston/proprietary/lib/libwms.so:system/lib/libwms.so \
+    vendor/orange/Boston/proprietary/lib/libnv.so:system/lib/libnv.so \
+    vendor/orange/Boston/proprietary/lib/libwmsts.so:system/lib/libwmsts.so \
+    vendor/orange/Boston/proprietary/lib/libdss.so:system/lib/libdss.so \
+    vendor/orange/Boston/proprietary/lib/libqmi.so:system/lib/libqmi.so \
+    vendor/orange/Boston/proprietary/lib/libdiag.so:system/lib/libdiag.so \
+    vendor/orange/Boston/proprietary/lib/libpbmlib.so:system/lib/libpbmlib.so \
+    vendor/orange/Boston/proprietary/lib/libauth.so:system/lib/libauth.so \
+    vendor/orange/Boston/proprietary/lib/liboem_rapi.so:system/lib/liboem_rapi.so \
+    vendor/orange/Boston/proprietary/lib/libdsm.so:system/lib/libdsm.so \
+    vendor/orange/Boston/proprietary/lib/libqueue.so:system/lib/libqueue.so \
+    vendor/orange/Boston/proprietary/lib/libcm.so:system/lib/libcm.so \
+    vendor/orange/Boston/proprietary/lib/libdll.so:system/lib/libdll.so \
+    vendor/orange/Boston/proprietary/lib/libril-qc-1.so:system/lib/libril-qc-1.so \
+    vendor/orange/Boston/proprietary/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so    
+
+## OMX proprietaries
+PRODUCT_COPY_FILES += \
+    vendor/orange/Boston/proprietary/lib/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
+    vendor/orange/Boston/proprietary/lib/libOmxAacDec.so:system/lib/libOmxAacDec.so \
+    vendor/orange/Boston/proprietary/lib/libOmxAmrRtpDec.so:system/lib/libOmxAmrRtpDec.so \
+    vendor/orange/Boston/proprietary/lib/libOmxH264Dec.so:system/lib/libOmxH264Dec.so \
+    vendor/orange/Boston/proprietary/lib/libOmxQcelpDec.so:system/lib/libOmxQcelpDec.so \
+    vendor/orange/Boston/proprietary/lib/libOmxAacEnc.so:system/lib/libOmxAacEnc.so \
+    vendor/orange/Boston/proprietary/lib/libOmxAmrwbDec.so:system/lib/libOmxAmrwbDec.so \
+    vendor/orange/Boston/proprietary/lib/libOmxMp3Dec.so:system/lib/libOmxMp3Dec.so \
+    vendor/orange/Boston/proprietary/lib/libOmxVidEnc.so:system/lib/libOmxVidEnc.so \
+    vendor/orange/Boston/proprietary/lib/libOmxAmrDec.so:system/lib/libOmxAmrDec.so \
+    vendor/orange/Boston/proprietary/lib/libOmxEvrcDec.so:system/lib/libOmxEvrcDec.so \
+    vendor/orange/Boston/proprietary/lib/libOmxMpeg4Dec.so:system/lib/libOmxMpeg4Dec.so \
+    vendor/orange/Boston/proprietary/lib/libOmxWmaDec.so:system/lib/libOmxWmaDec.so \
+    vendor/orange/Boston/proprietary/lib/libOmxAmrEnc.so:system/lib/libOmxAmrEnc.so \
+    vendor/orange/Boston/proprietary/lib/libOmxEvrcEnc.so:system/lib/libOmxEvrcEnc.so \
+    vendor/orange/Boston/proprietary/lib/libOmxQcelp13Enc.so:system/lib/libOmxQcelp13Enc.so \
+    vendor/orange/Boston/proprietary/lib/libOmxWmvDec.so:system/lib/libOmxWmvDec.so
+
+## Hardware properties 
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
+
+## Camera proprietaries
+PRODUCT_COPY_FILES += \
+    vendor/orange/Boston/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
+    vendor/orange/Boston/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
+    vendor/orange/Boston/proprietary/lib/libmmipl.so:system/lib/libmmipl.so
+
+## Atheros AR6002 firmware
+PRODUCT_COPY_FILES += \
+    device/orange/Boston/prebuilt/data.patch.hw2_0.bin:system/etc/firmware/data.patch.hw2_0.bin \
+    device/orange/Boston/prebuilt/eeprom.bin:system/etc/firmware/eeprom.bin \
+    device/orange/Boston/prebuilt/athwlan.bin.z77:system/etc/firmware/athwlan.bin.z77 \
+    device/orange/Boston/prebuilt/calData_ar6102_15dBm.bin:system/etc/firmware/calData_ar6102_15dBm.bin \
+    device/orange/Boston/hostapd.conf:system/etc/wifi/hostapd.conf \
+    vendor/orange/Boston/proprietary/bin/hostapd:system/bin/hostapd \
+    device/orange/Boston/prebuilt/wpa_supplicant:system/bin/wpa_supplicant
+
+## Other libraries and proprietary binaries
+PRODUCT_COPY_FILES += \
+    vendor/orange/Boston/proprietary/bin/hci_qcomm_init:system/bin/hci_qcomm_init \
+    device/orange/Boston/prebuilt/init.qcom.bt.sh:system/bin/init.qcom.bt.sh \
+    vendor/orange/Boston/proprietary/lib/libms3c_yamaha.so:system/lib/libms3c_yamaha.so \
+    vendor/orange/Boston/proprietary/lib/libsensor_yamaha.so:system/lib/libsensor_yamaha.so \
+    device/orange/Boston/ms3c_charger_offset.cfg:system/etc/ms3c_charger.cfg \
+    device/orange/Boston/ms3c_transformation.cfg:system/etc/ms3c_transform.cfg \
+    vendor/orange/Boston/proprietary/bin/updateSensorNV:system/bin/updateSensorNV \
+    vendor/orange/Boston/proprietary/bin/sensorserver_yamaha:system/bin/sensorserver_yamaha \
+    device/orange/Boston/prebuilt/sensordaemon:system/bin/sensordaemon \
+    vendor/orange/Boston/proprietary/lib/hw/sensors.qcom.so:system/lib/hw/sensors.qcom.so \
+    vendor/orange/Boston/proprietary/bin/gsensorcalibration:system/bin/gsensorcalibration \
+    device/orange/Boston/prebuilt/SensorCalibration.apk:system/app/SensorCalibration.apk \
+    device/orange/Boston/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
+    device/orange/Boston/AudioFilter.csv:system/etc/AudioFilter.csv \
+    vendor/orange/Boston/proprietary/lib/liba2dp.so:system/lib/liba2dp.so \
+    vendor/orange/Boston/proprietary/lib/libaudioeq.so:system/lib/libaudioeq.so \
+    vendor/orange/Boston/proprietary/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
+    vendor/orange/Boston/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    vendor/orange/Boston/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+    vendor/orange/Boston/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+    vendor/orange/Boston/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    vendor/orange/Boston/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    vendor/orange/Boston/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+    vendor/orange/Boston/proprietary/lib/libgsl.so:system/lib/libgsl.so
+
+PRODUCT_COPY_FILES += \
+    device/orange/Boston/media_profiles.xml:system/etc/media_profiles.xml \
+    device/orange/Boston/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    device/orange/Boston/vold.fstab:system/etc/vold.fstab \
+    device/orange/Boston/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/orange/Boston/7x27_kybd.kl:system/usr/keylayout/7x27_kybd.kl
+
+
+$(call inherit-product, build/target/product/full_base.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := Boston
+PRODUCT_DEVICE := Boston
+PRODUCT_MODEL := Orange Boston
+
+#Mejoras de consumo de rild y wifi
+PRODUCT_PROPERTY_OVERRIDES = ro.wifi.channels=11
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib/libril-qc-1.so \
+    rild.libargs=-d /dev/smd0 \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=45 \
+    keyguard.no_require_sim=true \
+    ro.ril.hsxpa=2 \
+    ro.ril.hsupa.category=5 \
+    ro.ril.def.agps.mode=2 \
+    ro.ril.gprsclass=10 \
+    ro.com.google.locationfeatures=1 \
+    ro.setupwizard.enable_bypass=1 \
+    ro.media.dec.jpeg.memcap=20000000 \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y \
+    ro.compcache.default=0 \
+    ro.com.android.dateformat=dd-MM-yyyy \
+    ro.telephony.default_network=0
---
+++ liblights/lights.c
@@ -0,0 +1,394 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2010-2011 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+//#define LOG_NDEBUG 0
+#define LOG_TAG "lights"
+
+#include <cutils/log.h>
+
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+#include <sys/ioctl.h>
+#include <sys/types.h>
+
+#include <hardware/lights.h>
+
+/******************************************************************************/
+
+static pthread_once_t g_init = PTHREAD_ONCE_INIT;
+static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
+static int g_haveTrackballLight = 0;
+static struct light_state_t g_notification;
+static struct light_state_t g_battery;
+static struct light_state_t g_applied;
+static int g_backlight = 255;
+static int g_trackball = -1;
+static int g_buttons = 0;
+static int g_attention = 0;
+static int g_wimax = 0;
+static int g_caps = 0;
+static int g_func = 0;
+
+char const*const TRACKBALL_FILE
+        = "/sys/class/leds/jogball-backlight/brightness";
+
+char const*const RED_LED_FILE
+        = "/sys/class/leds/red/brightness";
+
+char const*const GREEN_LED_FILE
+        = "/sys/class/leds/green/brightness";
+
+char const*const BLUE_LED_FILE
+        = "/sys/class/leds/blue/brightness";
+
+char const*const LCD_FILE
+        = "/proc/driver/max8831";
+
+char const*const RED_FREQ_FILE
+        = "/sys/class/leds/red/device/grpfreq";
+
+char const*const RED_PWM_FILE
+        = "/sys/class/leds/red/device/grppwm";
+
+char const*const RED_BLINK_FILE
+        = "/sys/class/leds/red/blink";
+
+char const*const GREEN_BLINK_FILE
+        = "/sys/class/leds/green/blink";
+
+char const*const KEYBOARD_FILE
+        = "/sys/class/leds/keyboard-backlight/brightness";
+
+char const*const BUTTON_FILE
+        = "/sys/class/leds/button-backlight/brightness";
+
+/**
+ * device methods
+ */
+
+void init_globals(void)
+{
+    // init the mutex
+    pthread_mutex_init(&g_lock, NULL);
+
+    // figure out if we have the trackball LED or not
+    g_haveTrackballLight = (access(TRACKBALL_FILE, W_OK) == 0) ? 1 : 0;
+
+}
+
+static int
+write_int(char const* path, int value)
+{
+    int fd;
+    static int already_warned = 0;
+
+    fd = open(path, O_RDWR);
+    if (fd >= 0) {
+        char buffer[20];
+        int bytes = sprintf(buffer, "%d\n", value);
+        int amt = write(fd, buffer, bytes);
+        close(fd);
+        return amt == -1 ? -errno : 0;
+    } else {
+        if (already_warned == 0) {
+            LOGE("write_int failed to open %s\n", path);
+            already_warned = 1;
+        }
+        return -errno;
+    }
+}
+
+static int
+is_lit(struct light_state_t const* state)
+{
+    return state->color & 0x00ffffff;
+}
+
+static int
+handle_trackball_light_locked(struct light_device_t* dev)
+{
+    int mode = g_attention;
+
+    if (mode == 7 && g_backlight) {
+        mode = 0;
+    }
+    LOGV("%s g_backlight = %d, mode = %d, g_attention = %d\n",
+        __func__, g_backlight, mode, g_attention);
+
+    // If the value isn't changing, don't set it, because this
+    // can reset the timer on the breathing mode, which looks bad.
+    if (g_trackball == mode) {
+        return 0;
+    }
+
+    return write_int(TRACKBALL_FILE, mode);
+}
+
+static int
+rgb_to_brightness(struct light_state_t const* state)
+{
+    int color = state->color & 0x00ffffff;
+    return ((77*((color>>16)&0x00ff))
+            + (150*((color>>8)&0x00ff)) + (29*(color&0x00ff))) >> 8;
+}
+
+static int
+set_light_backlight(struct light_device_t* dev,
+        struct light_state_t const* state)
+{
+    int err = 0;
+    int brightness = rgb_to_brightness(state);
+    pthread_mutex_lock(&g_lock);
+    g_backlight = brightness;
+    err = write_int(LCD_FILE, (brightness/2));
+    if (g_haveTrackballLight) {
+        handle_trackball_light_locked(dev);
+    }
+    pthread_mutex_unlock(&g_lock);
+    return err;
+}
+
+static int
+set_light_keyboard(struct light_device_t* dev,
+        struct light_state_t const* state)
+{
+    int err = 0;
+    int on = is_lit(state);
+    pthread_mutex_lock(&g_lock);
+    err = write_int(KEYBOARD_FILE, on?255:0);
+    pthread_mutex_unlock(&g_lock);
+    return err;
+}
+
+static int
+set_light_buttons(struct light_device_t* dev,
+        struct light_state_t const* state)
+{
+    int err = 0;
+    int on = is_lit(state);
+    pthread_mutex_lock(&g_lock);
+    g_buttons = on;
+    err = write_int(BUTTON_FILE, on?255:0);
+    pthread_mutex_unlock(&g_lock);
+    return err;
+}
+
+static int
+set_front_light_locked(struct light_device_t* dev,
+        struct light_state_t const* state)
+{
+    int len;
+    int alpha, red, green, blue;
+    int blink = 0;
+    int onMS, offMS;
+    unsigned int colorRGB;
+
+    switch (state->flashMode) {
+        case LIGHT_FLASH_TIMED:
+            onMS = state->flashOnMS;
+            offMS = state->flashOffMS;
+            break;
+        case LIGHT_FLASH_NONE:
+        default:
+            onMS = 0;
+            offMS = 0;
+            break;
+    }
+
+    colorRGB = state->color;
+
+    LOGV("set_front_light_locked colorRGB=%08X, onMS=%d, offMS=%d\n",
+            colorRGB, onMS, offMS);
+
+    red = (colorRGB >> 16) & 0xFF;
+    green = (colorRGB >> 8) & 0xFF;
+    blue = colorRGB & 0xFF;
+    LOGV("set_front_light RGB=0x%x, red=0x%x, green=0x%x, blue=0x%x, onMS=%d, offMS=%d",colorRGB, red,green,blue,onMS,offMS);
+
+    if (red) {
+        write_int(RED_LED_FILE, 1);
+        write_int(RED_BLINK_FILE, 0);
+    } else {
+        write_int(RED_LED_FILE, 0);
+        write_int(RED_BLINK_FILE, 0);
+    }
+
+    if (green) {
+        write_int(GREEN_LED_FILE, 1);
+        write_int(GREEN_BLINK_FILE, 0);
+    } else {
+        write_int(GREEN_LED_FILE, 0);
+        write_int(GREEN_BLINK_FILE, 0);
+    }
+
+    if (onMS > 0 && offMS > 0)
+        blink = 1;
+
+    if (blink) {
+        if (red && onMS != 0x1337) {
+            write_int(RED_LED_FILE, 0);
+            write_int(RED_BLINK_FILE, 1);
+        }
+        if (green && onMS != 0x1338) {
+            write_int(GREEN_LED_FILE, 0);
+            write_int(GREEN_BLINK_FILE, 1);
+        }
+    }
+
+    /* Always blink power button if there's a notification */
+    write_int("/sys/bus/i2c/devices/0-0045/powerbtn", blink ? 1 : 0);
+
+    return 0;
+}
+
+static void
+handle_front_battery_locked(struct light_device_t* dev)
+{
+    if (is_lit(&g_battery)) {
+        unsigned int newRGB = 0;
+        int red = (g_battery.color >> 16) & 0xFF;
+        int green = (g_battery.color >> 8) & 0xFF;
+
+        g_applied.flashOnMS = g_notification.flashOnMS;
+        g_applied.flashMode = g_notification.flashMode;
+
+        if (!red)
+            red = (g_notification.color >> 16) & 0xFF;
+        else
+            g_applied.flashOnMS = 0x1337;
+        if (!green)
+            green = (g_notification.color >> 8) & 0xFF;
+        else
+            g_applied.flashOnMS = 0x1338;
+
+        g_applied.flashOffMS = g_notification.flashOffMS;
+
+        newRGB += 0xFF<<24;
+        newRGB += (red ? 0xFF<<16 : 0);
+        newRGB += (green ? 0xFF<<8 : 0);
+        g_applied.color = newRGB;
+    } else {
+        g_applied = g_notification;
+    }
+    set_front_light_locked(dev, &g_applied);
+}
+
+static int
+set_light_notifications(struct light_device_t* dev,
+        struct light_state_t const* state)
+{
+    pthread_mutex_lock(&g_lock);
+    g_notification = *state;
+    LOGV("set_light_notifications g_trackball=%d color=0x%08x",
+            g_trackball, state->color);
+    if (g_haveTrackballLight) {
+        handle_trackball_light_locked(dev);
+    }
+    handle_front_battery_locked(dev);
+    pthread_mutex_unlock(&g_lock);
+    return 0;
+}
+
+static int
+set_light_attention(struct light_device_t* dev,
+        struct light_state_t const* state)
+{
+    return 0;
+}
+
+
+/** Close the lights device */
+static int
+close_lights(struct light_device_t *dev)
+{
+    if (dev) {
+        free(dev);
+    }
+    return 0;
+}
+
+
+/******************************************************************************/
+
+/**
+ * module methods
+ */
+
+/** Open a new instance of a lights device using name */
+static int open_lights(const struct hw_module_t* module, char const* name,
+        struct hw_device_t** device)
+{
+    int (*set_light)(struct light_device_t* dev,
+            struct light_state_t const* state);
+
+    if (0 == strcmp(LIGHT_ID_BACKLIGHT, name)) {
+        set_light = set_light_backlight;
+    }
+    else if (0 == strcmp(LIGHT_ID_KEYBOARD, name)) {
+        set_light = set_light_keyboard;
+    }
+    else if (0 == strcmp(LIGHT_ID_BUTTONS, name)) {
+        set_light = set_light_buttons;
+    }
+    else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name)) {
+        set_light = set_light_notifications;
+    }
+    else if (0 == strcmp(LIGHT_ID_ATTENTION, name)) {
+        set_light = set_light_attention;
+    }
+    else {
+        return -EINVAL;
+    }
+
+    pthread_once(&g_init, init_globals);
+
+    struct light_device_t *dev = malloc(sizeof(struct light_device_t));
+    memset(dev, 0, sizeof(*dev));
+
+    dev->common.tag = HARDWARE_DEVICE_TAG;
+    dev->common.version = 0;
+    dev->common.module = (struct hw_module_t*)module;
+    dev->common.close = (int (*)(struct hw_device_t*))close_lights;
+    dev->set_light = set_light;
+
+    *device = (struct hw_device_t*)dev;
+    return 0;
+}
+
+
+static struct hw_module_methods_t lights_module_methods = {
+    .open =  open_lights,
+};
+
+/*
+ * The lights Module
+ */
+const struct hw_module_t HAL_MODULE_INFO_SYM = {
+    .tag = HARDWARE_MODULE_TAG,
+    .version_major = 1,
+    .version_minor = 0,
+    .id = LIGHTS_HARDWARE_MODULE_ID,
+    .name = "Commtiva z71 lights Module",
+    .author = "CyanogenMod Project",
+    .methods = &lights_module_methods,
+};
